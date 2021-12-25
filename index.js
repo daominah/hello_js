fafc8b2b465bcbba66a826ac3db9bf2f0786c542
DaoMinAh <tung.daothanhtung@gmail.com>
2021-12-25 16:58:49 +0700

    add loadArrayToTable2 vertical

--- index.js
+++ index.js
@@ -49,3 +49,40 @@ function loadArrayToTable(tableElem, array) {
     tableElem.innerHTML = "";
     tableElem.appendChild(tbody);
 }
+
+function loadArrayToTable2(tableElem, array) {
+	if (array.length < 1) {
+		console.log("empty data:", array);
+		return
+	}
+	let keys = Object.keys(array[0]);
+
+	let tbody = document.createElement("tbody");
+	let tr = document.createElement("tr");
+	for (let col = 0; col < keys.length; col ++) {
+		let bold = document.createElement("strong");
+		bold.appendChild(document.createTextNode(keys[col]));
+		let tdKey = document.createElement("td");
+		tdKey.appendChild(bold);
+		tr.appendChild(tdKey);
+	}
+	tbody.appendChild(tr);
+
+	for (let row = 0; row < array.length; row++) {
+		let tr = document.createElement("tr");
+		for (let col = 0; col < keys.length; col++) {
+			let td = document.createElement("td");
+			let value = array[row][keys[col]];
+			if (!value) {
+				tr.appendChild(td);
+				continue
+			}
+			td.appendChild(document.createTextNode(value));
+			tr.appendChild(td)
+		}
+		tbody.appendChild(tr);
+	}
+
+	tableElem.innerHTML = "";
+	tableElem.appendChild(tbody);
+}
